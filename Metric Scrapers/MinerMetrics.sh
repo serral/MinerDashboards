57c16401943b36aca06b7f8bdcc445cb4ad4c5ae
Andre Serralheiro <167178+serral@users.noreply.github.com>
2024-04-01 01:40:30 +0100

    update to InfluxDB2

--- Metric Scrapers/MinerMetrics.sh
+++ Metric Scrapers/MinerMetrics.sh
@@ -13,11 +13,11 @@
 # HashRate=$(echo $var1)
 # UpTime=$(echo $var3)
 
-MinerStats=$(curl -X GET -H "Content-Type: application/json" http://<IP>:<PORT>/1/summary)
+MinerStats=$(curl -X GET -H "Content-Type: application/json" http://<IP>:<PORT>/2/summary)
 
 HashRate=$(echo $MinerStats | jq -r '.hashrate.total[0]')
 Miner=$(echo $MinerStats | jq -r '.worker_id')
 Algo=$(echo $MinerStats | jq -r '.algo')
 UpTime=$(echo $MinerStats | jq -r '.uptime')
 
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "MinerMetrics,Miner=$Miner,Algo=$Algo UpTime=$UpTime,HashRate=$HashRate"
+curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "MinerMetrics,Miner=$Miner,Algo=$Algo UpTime=$UpTime,HashRate=$HashRate"
--- Metric Scrapers/ProxyMetrics.sh
+++ Metric Scrapers/ProxyMetrics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #ProxyMetrics.sh
 
-ProxyStats=$(curl -X GET -H "Content-Type: application/json" http://<IP>:<PORT>/1/summary)
+ProxyStats=$(curl -X GET -H "Content-Type: application/json" http://<IP>:<PORT>/2/summary)
 
 MinerCount=$(echo $ProxyStats | jq -r '.miners.now')
 UpTime=$(echo $ProxyStats | jq -r '.uptime')
 
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "ProxyMetrics MinerCount=$MinerCount,UpTime=$UpTime"
+curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "ProxyMetrics MinerCount=$MinerCount,UpTime=$UpTime"
