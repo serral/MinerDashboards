ddb9079aac3cfd3801e23d7c9ee657686c621daa
Andre Serralheiro <167178+serral@users.noreply.github.com>
2025-03-09 14:02:45 +0000

    update

--- Metric Scrapers/WalletMetrics.sh
+++ Metric Scrapers/WalletMetrics.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #WalletMetrics.sh
 
-WalletBalance=$(curl http://<IP>:<PORT>/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_balance","params":{"account_index":0,"address_indices":[0,1]}}' -H 'Content-Type: application/json')
+
+#WalletBalance=$(curl http://<IP>:<PORT>/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_balance","params":{"account_index":0,"address_indices":[0,1]}}' -H 'Content-Type: application/json')
+WalletBalance=$(curl http://10.0.0.105:18083/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"get_balance","params":{"account_index":0,"address_indices":[0]}}' -H 'Content-Type: application/json')
+#address_indices array may vary. Check output of monero-wallet-rpc
+
 
 WalletBalance=$(echo $WalletBalance | jq -r '.result.balance')
 WalletBalance=$(echo "$WalletBalance * 0.000000000001" | bc)
 
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "WalletBalance,Coin=XMR WalletBalance=$WalletBalance"
+curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "WalletBalance,Coin=XMR WalletBalance=$WalletBalance"
--- WORKLOG.md
+++ WORKLOG.md
@@ -28,15 +28,16 @@ Note: Configure all variables within ```< >``` to reflect your settings.
 ```shell
 #!/bin/bash
 apt -y update
-echo "y" | sudo ufw enable
-ufw allow 8086/tcp
+#echo "y" | sudo ufw enable
+#ufw allow 8086/tcp
 echo "deb https://repos.influxdata.com/ubuntu focal stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 apt -y install curl
 apt -y install jq
-apt -y install influxdb
+#apt -y install influxdb
+apt -y install influxdb2
 apt -y install influxdb-client
 service influxdb start
 curl -POST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE MoneroMetrics WITH DURATION 30d"
