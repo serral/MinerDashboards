1f1b7df748818599697367bb6ed3e7e2d7c7726e
Andre Serralheiro <167178+serral@users.noreply.github.com>
2025-03-09 14:02:45 +0000

    update

--- Metric Scrapers/PoolMetrics.sh
+++ Metric Scrapers/PoolMetrics.sh
@@ -2,9 +2,10 @@
 #PoolMetrics.sh
 
 PoolHashRate=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/pool/stats)
-MinerHashRate=$(curl https://api.moneroocean.stream/miner/<ADDRESS>/chart/hashrate)
-LastPayment=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/miner/<ADDRESS>/payments)
-AmountDue=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/miner/<ADDRESS>/stats)
+MinerHashRate=$(curl https://api.moneroocean.stream/miner/${XMR_ADDRESS}/chart/hashrate)
+LastPayment=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/miner/${XMR_ADDRESS}/payments)
+AmountDue=$(curl -X GET -H "Content-Type: application/json" https://api.moneroocean.stream/miner/${XMR_ADDRESS}/stats)
+
 
 PoolHashRate=$(echo $PoolHashRate | jq '.pool_statistics.hashRate')
 MinerHashRate=$(echo $MinerHashRate | jq '.[0].hs2')
@@ -20,4 +21,5 @@ if [ -z "$LastPayment" ]; then
 LastPayment=0
 fi
 
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "PoolMetrics,Pool=MoneroOcean PoolHashRate=$PoolHashRate,MinerHashRate=$MinerHashRate,LastPayment=$LastPayment,AmountDue=$AmountDue"
+#curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "PoolMetrics,Pool=MoneroOcean PoolHashRate=$PoolHashRate,MinerHashRate=$MinerHashRate,LastPayment=$LastPayment,AmountDue=$AmountDue"
+curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "PoolMetrics,Pool=MoneroOcean PoolHashRate=$PoolHashRate,MinerHashRate=$MinerHashRate,LastPayment=$LastPayment,AmountDue=$AmountDue"
--- Metric Scrapers/XMRValue.sh
+++ Metric Scrapers/XMRValue.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 #XMRValue.sh
 
-XMRMetrics=$(curl -X GET -H "X-CMC_PRO_API_KEY: <API_KEY>" -H "Accept: application/json" -d "symbol=XMR" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest)
+#XMRMetrics=$(curl -X GET -H "X-CMC_PRO_API_KEY: <API_KEY>" -H "Accept: application/json" -d "symbol=XMR" -G https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest)
+XMRMetrics=$(curl -X GET -H "X-CMC_PRO_API_KEY: 90c4bba5-b422-444e-8a2b-90e79eb930df" -H "Accept: application/json" -d "symbol=XMR" -G https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest)
 
-XMRValue=$(echo $XMRMetrics | jq '.data.XMR.quote.USD.price')
-DayChange=$(echo $XMRMetrics | jq '.data.XMR.quote.USD.percent_change_24h')
+XMRValue=$(echo $XMRMetrics | jq '.data.XMR [] .quote.USD.price')
+DayChange=$(echo $XMRMetrics | jq '.data.XMR [] .quote.USD.percent_change_24h')
+HourChange=$(echo $XMRMetrics | jq '.data.XMR [] .quote.USD.percent_change_1h')
 
-curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange"
+
+#curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange"
+
+#curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange"
+curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange,HourChange=$HourChange"
+
+
+echo XMRValue
+echo $XMRValue
+echo DayChange
+echo $DayChange
+echo  HourChange
+echo $HourChange
