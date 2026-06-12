4e6d8feebb09e2b401a1d84de977f1e516dd1eee
Andre Serralheiro <167178+serral@users.noreply.github.com>
2026-06-12 20:23:21 +0100

    Move CMC API key to env var and harden XMRValue.sh

--- Metric Scrapers/XMRValue.sh
+++ Metric Scrapers/XMRValue.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 #XMRValue.sh
 
-#XMRMetrics=$(curl -X GET -H "X-CMC_PRO_API_KEY: <API_KEY>" -H "Accept: application/json" -d "symbol=XMR" -G https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest)
-XMRMetrics=$(curl -X GET -H "X-CMC_PRO_API_KEY: REDACTED_CMC_KEY" -H "Accept: application/json" -d "symbol=XMR" -G https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest)
+# Requires: CMC_PRO_API_KEY, INFLUX_HOST, INFLUX_ORG, INFLUX_BUCKET, INFLUX_TOKEN
 
-XMRValue=$(echo $XMRMetrics | jq '.data.XMR [] .quote.USD.price')
-DayChange=$(echo $XMRMetrics | jq '.data.XMR [] .quote.USD.percent_change_24h')
-HourChange=$(echo $XMRMetrics | jq '.data.XMR [] .quote.USD.percent_change_1h')
+XMRMetrics=$(curl -s -X GET \
+  -H "X-CMC_PRO_API_KEY: ${CMC_PRO_API_KEY}" \
+  -H "Accept: application/json" \
+  -d "symbol=XMR" -G \
+  https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest)
 
+XMRValue=$(echo "$XMRMetrics" | jq '.data.XMR[].quote.USD.price')
+DayChange=$(echo "$XMRMetrics" | jq '.data.XMR[].quote.USD.percent_change_24h')
+HourChange=$(echo "$XMRMetrics" | jq '.data.XMR[].quote.USD.percent_change_1h')
 
-#curl -i -XPOST 'http://<IP>:<PORT>/write?db=MoneroMetrics' --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange"
+# Skip the write if the API call failed (key/rate-limit/network)
+if [ -z "$XMRValue" ] || [ "$XMRValue" = "null" ]; then
+    echo "XMRValue.sh: no price returned, skipping write" >&2
+    exit 1
+fi
 
-#curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange"
-curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" --header "Authorization: Token ${INFLUX_TOKEN}" --header "Content-Type: text/plain; charset=utf-8" --header "Accept: application/json" --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange,HourChange=$HourChange"
-
-
-echo XMRValue
-echo $XMRValue
-echo DayChange
-echo $DayChange
-echo  HourChange
-echo $HourChange
+curl -XPOST "http://${INFLUX_HOST}/api/v2/write?org=${INFLUX_ORG}&bucket=${INFLUX_BUCKET}&precision=ns" \
+  --header "Authorization: Token ${INFLUX_TOKEN}" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary "XMRValue XMRValue=$XMRValue,DayChange=$DayChange,HourChange=$HourChange"
